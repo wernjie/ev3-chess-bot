4411966ff27469e205e01a55d29cc8bca12618bf
Wern Jie Lim <lwjdev@gmail.com>
2021-11-29 11:38:31 +0800

    updated to stockfish 14

--- engine/server.js
+++ engine/server.js
@@ -3,7 +3,13 @@ var http = require('http');
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 
-var serve = serveStatic("./");
+var serve = serveStatic("./", {
+  setHeaders: function(res, path) {
+    res.setHeader('Cross-Origin-Resource-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  }
+});
 
 var server = http.createServer(function(req, res) {
   var done = finalhandler(req, res);
